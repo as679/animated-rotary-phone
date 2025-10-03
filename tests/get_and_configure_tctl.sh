c0e6c1760153b2b9ad519adc5dd87e97c13d7684
Adrian Steer <adrian.steer@tetrate.io>
2025-10-03 16:43:40 +0100

    Building out kind test workflow

--- tests/get_and_configure_tctl.sh
+++ tests/get_and_configure_tctl.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -env
+set -e
 
-VER=${3:-'1.13.0'}
 TSB_ADDRESS=$1
 TSB_ADMIN_PASSWORD=$2
+VER=${3:-'1.13.0'}
 
-./get_tctl.sh -v ${VER}
+sudo ./get_tctl.sh -v ${VER}
 
 tctl config clusters set default --bridge-address ${TSB_ADDRESS}
 tctl config users set default --username admin --password ${TSB_ADMIN_PASSWORD} --org apidemo
